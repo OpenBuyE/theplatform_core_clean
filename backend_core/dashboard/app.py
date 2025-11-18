6d33cc97e2c144460e3bd28f10845f438ef4b3dc
OpenBuyE <compraabierta.admin@protonmail.com>
2025-11-18 12:41:19 +0100

    Add dashboard main application

---
+++ backend_core/dashboard/app.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+from backend_core.dashboard.ui.layout import (
+    setup_page,
+    render_header,
+    render_sidebar
+)
+
+from backend_core.dashboard.views.park_sessions import render_park_sessions
+from backend_core.dashboard.views.active_sessions import render_active_sessions
+from backend_core.dashboard.views.chains import render_chains
+
+
+def main():
+    setup_page()
+    render_header()
+    choice = render_sidebar()
+
+    if choice == "Parque de Sesiones":
+        render_park_sessions()
+    elif choice == "Sesiones Activas":
+        render_active_sessions()
+    elif choice == "Cadenas":
+        render_chains()
+
+
+if __name__ == "__main__":
+    main()
