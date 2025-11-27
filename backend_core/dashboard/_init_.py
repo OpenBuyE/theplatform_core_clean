85c20b69e1bf95526888ec4fb027ad26461ef584
OpenBuyE <compraabierta.admin@protonmail.com>
2025-11-27 10:14:32 +0100

    Update _init_.py

--- backend_core/dashboard/_init_.py
+++ backend_core/dashboard/_init_.py
@@ -1,2 +1,7 @@
-from .app import main
+# backend_core/dashboard/__init__.py
+"""
+Dashboard package initializer.
+Permite que Python reconozca este folder como módulo.
+"""
 
+from .app import main  # para permitir import main desde otros módulos
